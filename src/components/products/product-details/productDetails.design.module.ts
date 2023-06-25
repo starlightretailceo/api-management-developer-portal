3bdd1c60282d6731763396fb88af6e0d48ea6587
Alexander Zaslonov <alzaslon@microsoft.com>
2023-06-24 20:23:08 -0700

    tmp

--- src/components/products/product-details/productDetails.design.module.ts
+++ src/components/products/product-details/productDetails.design.module.ts
@@ -1,9 +1,36 @@
 import { IInjectorModule, IInjector } from "@paperbits/common/injection";
 import { ProductDetailsHandlers } from "./productDetailsHandlers";
+import { ProductDetailsModel } from "./productDetailsModel";
+import { KnockoutComponentBinder } from "@paperbits/core/ko";
+import { ProductDetailsModelBinder } from "./productDetailsModelBinder";
+import { ProductDetailsViewModelBinder } from "./ko/productDetailsViewModelBinder";
+import { ProductDetailsViewModel } from "./ko/productDetailsViewModel";
+import { IWidgetService } from "@paperbits/common/widgets";
 
 export class ProductDetailsDesignModule implements IInjectorModule {
     public register(injector: IInjector): void {
+        // injector.bind("productApisEditor", ProductDetailsEditor);
+        injector.bindSingleton("productDetailsModelBinder", ProductDetailsModelBinder);
+        injector.bindSingleton("productDetailsViewModelBinder", ProductDetailsViewModelBinder)
         injector.bindSingleton("productDetailsHandlers", ProductDetailsHandlers);
-        injector.bindToCollection("widgetHandlers", ProductDetailsHandlers);
+
+        const widgetService = injector.resolve<IWidgetService>("widgetService");
+
+        widgetService.registerWidget("productDetails", {
+            modelDefinition: ProductDetailsModel,
+            componentBinder: KnockoutComponentBinder,
+            componentDefinition: ProductDetailsViewModel,
+            modelBinder: ProductDetailsModelBinder,
+            viewModelBinder: ProductDetailsViewModelBinder
+        });
+
+        widgetService.registerWidgetEditor("productDetails", {
+            displayName: "Product Details",
+            category: "Products",
+            iconClass: "widget-icon widget-icon-api-management",
+            componentBinder: KnockoutComponentBinder,
+            componentDefinition: null, // ProductDetailsEditor,
+            handlerComponent: ProductDetailsHandlers
+        });
     }
 }
