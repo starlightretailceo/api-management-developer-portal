882fbd63bedcc3aa48af2f22c1a6af5b290ea17c
Alexander Zaslonov <alzaslon@microsoft.com>
2023-06-24 20:29:55 -0700

    tmp

--- src/components/products/product-details/ko/productDetailsViewModelBinder.ts
+++ src/components/products/product-details/ko/productDetailsViewModelBinder.ts
@@ -7,37 +7,6 @@ import { ProductDetailsViewModel } from "./productDetailsViewModel";
 export class ProductDetailsViewModelBinder implements ViewModelBinder<ProductDetailsModel, ProductDetailsViewModel> {
     constructor(private readonly styleCompiler: StyleCompiler) { }
 
-    // public async modelToViewModel(model: ProductDetailsModel, viewModel?: ProductDetailsViewModel, bindingContext?: Bag<any>): Promise<ProductDetailsViewModel> {
-    //     if (!viewModel) {
-    //         viewModel = new ProductDetailsViewModel();
-    //     }
-
-    //     viewModel["widgetBinding"] = {
-    //         displayName: "Product: Details",
-    //         layer: bindingContext?.layer,
-    //         model: model,
-    //         draggable: true,
-    //         handler: ProductDetailsHandlers,
-    //         flow: ComponentFlow.Block,
-    //         applyChanges: async (updatedModel: ProductDetailsModel) => {
-    //             await this.modelToViewModel(updatedModel, viewModel, bindingContext);
-    //             this.eventManager.dispatchEvent(Events.ContentUpdate);
-    //         }
-    //     };
-
-    //     if (model.styles) {
-    //         viewModel.styles(await this.styleCompiler.getStyleModelAsync(model.styles, bindingContext?.styleManager));
-    //     }
-
-    //     return viewModel;
-    // }
-
-    // public canHandleModel(model: ProductDetailsModel): boolean {
-    //     return model instanceof ProductDetailsModel;
-    // }
-
-
-
     public stateToInstance(state: WidgetState, componentInstance: ProductDetailsViewModel): void {
         componentInstance.styles(state.styles);
     }
