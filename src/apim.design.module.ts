e57a1124f5dbc1e43b491c6b01fa2d88f9de6566
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2023-07-08 16:07:25 -0700

    Migrated "History Of Api".

--- src/apim.design.module.ts
+++ src/apim.design.module.ts
@@ -12,7 +12,6 @@ import { RoleBasedSecurityDesignModule } from "@paperbits/core/security/roleBase
 import { ListOfApisEditorModule } from "./components/apis/list-of-apis/ko/listOfApisEditor.module";
 import { DetailsOfApiModule } from "./components/apis/details-of-api/ko/detailsOfApi.module";
 import { DetailsOfApiEditorModule } from "./components/apis/details-of-api/ko/detailsOfApiEditor.module";
-import { HistoryOfApiModule } from "./components/apis/history-of-api/ko/historyOfApi.module";
 import { HistoryOfApiEditorModule } from "./components/apis/history-of-api/ko/historyOfApiEditor.module";
 import { SigninModule } from "./components/users/signin/signin.module";
 import { SigninDesignModule } from "./components/users/signin/signin.design.module";
@@ -75,7 +74,6 @@ export class ApimDesignModule implements IInjectorModule {
         injector.bindModule(new ApiProductsEditorModule());
         injector.bindModule(new DetailsOfApiModule());
         injector.bindModule(new DetailsOfApiEditorModule());
-        injector.bindModule(new HistoryOfApiModule());
         injector.bindModule(new HistoryOfApiEditorModule());
         injector.bindModule(new SigninModule());
         injector.bindModule(new SigninDesignModule());
--- src/components/apis/history-of-api/historyOfApiHandlers.ts
+++ src/components/apis/history-of-api/historyOfApiHandlers.ts
@@ -1,20 +1,11 @@
-import { IWidgetOrder, IWidgetHandler } from "@paperbits/common/editing";
+import { IWidgetHandler } from "@paperbits/common/editing";
 import { StyleDefinition } from "@paperbits/common/styles";
 import { HistoryOfApiModel } from "./historyOfApiModel";
 import * as DefaultStyleDefinitions from "../../defaultStyleDefinitions";
 
 export class HistoryOfApiHandlers implements IWidgetHandler {
-    public async getWidgetOrder(): Promise<IWidgetOrder> {
-        const widgetOrder: IWidgetOrder = {
-            name: "apiHistory",
-            category: "APIs",
-            displayName: "API: Change history",
-            iconClass: "widget-icon widget-icon-api-management",
-            requires: ["html"],
-            createModel: async () => new HistoryOfApiModel()
-        };
-
-        return widgetOrder;
+    public async getWidgetModel(): Promise<HistoryOfApiModel> {
+        return new HistoryOfApiModel();
     }
 
     public getStyleDefinitions(): StyleDefinition {
